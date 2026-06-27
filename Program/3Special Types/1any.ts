29cb5e460e880dc5c04d73560b86885472902a04
UtkarshS-cmd <sharmautkarsh924@gmail.com>
2026-06-27 13:27:16 +0530

    typescirpt compilation in jupyter notebook

--- Program/3Special Types/1any.ts
+++ Program/3Special Types/1any.ts
@@ -1,4 +1,7 @@
-let v : any = true;
+let v: any = true;
+console.log(typeof (v))
 v = "String"
 let rounded_any = Math.round(v);
 console.log(rounded_any) //nan
+
+console.log(typeof (v))
--- Program/practice.ts
+++
@@ -1,8 +0,0 @@
-let x: unknown = "Hello"
-console.log((x as string).length)
-
-let y: unknown = "gseg"
-console.log((<string>y))
-
-let z = "65"
-console.log(((z as unknown) as number))
