708ff80ada6be7eef4c13d777bc706b17b8a41da
UtkarshS-cmd <sharmautkarsh924@gmail.com>
2026-06-28 09:56:15 +0530

    debugging

--- Scrimble-Typescript/1pizzaApp.ts
+++ Scrimble-Typescript/1pizzaApp.ts
@@ -1,21 +1,20 @@
 type Pizza = {
+    id: number
     name: string
     price: number
 }
 
-type Status = "ordered" | "completed"
-
 type Order = {
     id: number
     pizza: Pizza
-    status: Status
+    status: "ordered" | "completed"
 }
 
-const menu = [
-    { name: "Margherita", price: 8 },
-    { name: "Pepperoni", price: 10 },
-    { name: "Hawaiian", price: 10 },
-    { name: "Veggie", price: 9 },
+const menu: Pizza[] = [
+    { id: 1, name: "Margherita", price: 8 },
+    { id: 2, name: "Pepperoni", price: 10 },
+    { id: 3, name: "Hawaiian", price: 10 },
+    { id: 4, name: "Veggie", price: 9 },
 ]
 
 let cashInRegister = 100
@@ -48,12 +47,16 @@ function completeOrder(orderId: number) {
     return order
 }
 
-addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({ name: "BBQ Chicken", price: 12 })
-addNewPizza({ name: "Spicy Sausage", price: 11 })
+addNewPizza({ id: 5, name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ id: 6, name: "BBQ Chicken", price: 12 })
+addNewPizza({ id: 7, name: "Spicy Sausage", price: 11 })
 
 placeOrder("Chicken Bacon Ranch")
+placeOrder("Pepperoni")
 completeOrder(1)
+placeOrder("Anchovy")
+placeOrder("Veggie")
+completeOrder(2)
 
 console.log("Menu:", menu)
 console.log("Cash in register:", cashInRegister)
